c39a16e01abb5266729b700ffd9ce123d5ef2957
Kevin McKee <kevin.mckee@procore.com>
2018-08-16 17:57:55 -0700

    Initial Commit

---
+++ ProcoreUserReports/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProcoreUserReports
+//
+//  Created by Kevin McKee on 8/16/18.
+//  Copyright © 2018 Procore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
